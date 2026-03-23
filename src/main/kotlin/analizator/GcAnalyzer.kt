78913b97e0b6d4ef24062fa8cdcaf8a5abe0c906
Раиса Чухнова <Raya.chuhnova@yandex.ru>
2026-03-23 23:53:30 +0300

    Implement GcAnalyzer class for sequence analysis

--- src/main/kotlin/analizator/GcAnalyzer.kt
+++ src/main/kotlin/analizator/GcAnalyzer.kt
@@ -1,1 +1,28 @@
+package analizator
 
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+class GcAnalyzer {
+    fun analyze(sequence: String): SequenceStats {
+        require(sequence.isNotEmpty()) { "Последовательность пустая" }
+
+        val aCount = sequence.count { it == 'A' }
+        val tCount = sequence.count { it == 'T' }
+        val gCount = sequence.count { it == 'G' }
+        val cCount = sequence.count { it == 'C' }
+        val length = sequence.length
+        val gcPercent = BigDecimal((gCount + cCount) * 100.0 / length)
+            .setScale(2, RoundingMode.HALF_UP)
+            .toDouble()
+
+        return SequenceStats(
+            length = length,
+            aCount = aCount,
+            tCount = tCount,
+            gCount = gCount,
+            cCount = cCount,
+            gcPercent = gcPercent
+        )
+    }
+}
