522db0ceb82a0091209978e3383ae5763b2eaeb5
Раиса Чухнова <Raya.chuhnova@yandex.ru>
2026-03-24 00:19:41 +0300

    Add ApiMappings.kt for sequence report DTO conversion

---
+++ src/main/kotlin/analizator/ApiMappings.kt
@@ -0,0 +1,38 @@
+package analizator
+
+import analizator.dto.AnalyzeResponseDto
+import analizator.dto.OrfDto
+import analizator.dto.ProteinDto
+import analizator.dto.SequenceStatsDto
+
+fun SequenceReport.toDto(): AnalyzeResponseDto {
+    return AnalyzeResponseDto(
+        header = header,
+        sequence = sequence,
+        stats = SequenceStatsDto(
+            length = stats.length,
+            aCount = stats.aCount,
+            tCount = stats.tCount,
+            gCount = stats.gCount,
+            cCount = stats.cCount,
+            gcPercent = stats.gcPercent
+        ),
+        orfs = orfs.map {
+            OrfDto(
+                frame = it.frame,
+                start = it.start,
+                end = it.end,
+                length = it.length,
+                sequence = it.sequence
+            )
+        },
+        proteins = proteins.map {
+            ProteinDto(
+                frame = it.frame,
+                start = it.start,
+                end = it.end,
+                aminoAcidSequence = it.aminoAcidSequence
+            )
+        }
+    )
+}
