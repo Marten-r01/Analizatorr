1b4930f3006e8a16565dbefda194ad820cf55bed
Раиса Чухнова <Raya.chuhnova@yandex.ru>
2026-03-23 23:56:38 +0300

    Add unit test for GcAnalyzer functionality

--- src/test/kotlin/analizator/GcAnalyzerTest.kt
+++ src/test/kotlin/analizator/GcAnalyzerTest.kt
@@ -1,1 +1,20 @@
+package analizator
 
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class GcAnalyzerTest {
+    private val analyzer = GcAnalyzer()
+
+    @Test
+    fun calculateStatsCorrectly() {
+        val stats = analyzer.analyze("ATGAAATAGCCCATGCCCTAAATGTTTTGA")
+
+        assertEquals(30, stats.length)
+        assertEquals(10, stats.aCount)
+        assertEquals(9, stats.tCount)
+        assertEquals(5, stats.gCount)
+        assertEquals(6, stats.cCount)
+        assertEquals(36.67, stats.gcPercent)
+    }
+}
