4960c286bdee854fa48de5caef77a6d64a0fc72d
Раиса Чухнова <Raya.chuhnova@yandex.ru>
2026-03-24 00:10:42 +0300

    Update SequenceAnalysisService.kt

--- src/main/kotlin/analizator/SequenceAnalysisService.kt
+++ src/main/kotlin/analizator/SequenceAnalysisService.kt
@@ -3,18 +3,21 @@ package analizator
 class SequenceAnalysisService(
     private val parser: FastaParser,
     private val gcAnalyzer: GcAnalyzer,
-    private val orfFinder: OrfFinder
+    private val orfFinder: OrfFinder,
+    private val dnaTranslator: DnaTranslator
 ) {
     fun analyze(lines: List<String>): SequenceReport {
         val record = parser.parse(lines)
         val stats = gcAnalyzer.analyze(record.sequence)
         val orfs = orfFinder.find(record.sequence)
+        val proteins = dnaTranslator.translate(orfs)
 
         return SequenceReport(
             header = record.header,
             sequence = record.sequence,
             stats = stats,
-            orfs = orfs
+            orfs = orfs,
+            proteins = proteins
         )
     }
 }
