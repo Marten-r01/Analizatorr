0f3dafac187c5ca72fba34b9829233caffe0c28c
Раиса Чухнова <Raya.chuhnova@yandex.ru>
2026-03-23 23:52:21 +0300

    Implement FastaValidator class for FASTA validation

--- src/main/kotlin/analizator/FastaValidator.kt
+++ src/main/kotlin/analizator/FastaValidator.kt
@@ -1,1 +1,24 @@
+package analizator
 
+class FastaValidator {
+    private val allowedSymbols = setOf('A', 'T', 'G', 'C')
+
+    fun validateStructure(lines: List<String>) {
+        require(lines.isNotEmpty()) { "Файл пустой" }
+        require(lines.first().startsWith(">")) { "FASTA-заголовок должен начинаться с символа '>'" }
+        require(lines.count { it.startsWith(">") } == 1) { "На 2 неделе поддерживается только одна FASTA-запись" }
+    }
+
+    fun validateHeader(header: String) {
+        require(header.isNotBlank()) { "Заголовок FASTA пустой" }
+    }
+
+    fun validateSequence(sequence: String) {
+        require(sequence.isNotEmpty()) { "После заголовка отсутствует последовательность" }
+
+        val invalidIndex = sequence.indexOfFirst { it !in allowedSymbols }
+        require(invalidIndex == -1) {
+            "Обнаружен недопустимый символ '${sequence[invalidIndex]}' в позиции ${invalidIndex + 1}"
+        }
+    }
+}
