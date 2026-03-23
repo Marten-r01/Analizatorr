6b65c227ef6b134fa6c5f05b2dc8263e507c48b1
Раиса Чухнова <Raya.chuhnova@yandex.ru>
2026-03-23 23:55:56 +0300

    Add tests for FastaParser functionality

--- src/test/kotlin/analizator/FastaParserTest.kt
+++ src/test/kotlin/analizator/FastaParserTest.kt
@@ -1,1 +1,35 @@
+package analizator
 
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class FastaParserTest {
+    private val parser = FastaParser(FastaValidator())
+
+    @Test
+    fun parseMultilineFastaAndUppercaseSequence() {
+        val lines = listOf(
+            ">seq_1",
+            "atgc",
+            "ggta"
+        )
+
+        val record = parser.parse(lines)
+
+        assertEquals("seq_1", record.header)
+        assertEquals("ATGCGGTA", record.sequence)
+    }
+
+    @Test
+    fun failWhenSequenceContainsInvalidSymbol() {
+        val lines = listOf(
+            ">seq_1",
+            "ATGZ"
+        )
+
+        assertFailsWith<IllegalArgumentException> {
+            parser.parse(lines)
+        }
+    }
+}
