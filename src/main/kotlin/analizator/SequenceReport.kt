2666d1937d38b66fff91316a76aec52da3a02a54
Раиса Чухнова <Raya.chuhnova@yandex.ru>
2026-03-24 00:07:05 +0300

    Add proteins field to SequenceReport data class

--- src/main/kotlin/analizator/SequenceReport.kt
+++ src/main/kotlin/analizator/SequenceReport.kt
@@ -4,5 +4,7 @@ data class SequenceReport(
     val header: String,
     val sequence: String,
     val stats: SequenceStats,
-    val orfs: List<Orf>
+    val orfs: List<Orf>,
+    val proteins: List<ProteinTranslation>
+    
 )
