875efcfeb7bff450cca1b6d3324a9605d373d2bf
Раиса Чухнова <Raya.chuhnova@yandex.ru>
2026-03-23 23:49:39 +0300

    Update SequenceStats.kt

--- src/main/kotlin/analizator/SequenceStats.kt
+++ src/main/kotlin/analizator/SequenceStats.kt
@@ -1,1 +1,10 @@
+package analizator
 
+data class SequenceStats(
+    val length: Int,
+    val aCount: Int,
+    val tCount: Int,
+    val gCount: Int,
+    val cCount: Int,
+    val gcPercent: Double
+)
