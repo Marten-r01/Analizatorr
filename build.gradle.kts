e550393e2f58e46897fa57164d12594130444245
Раиса Чухнова <Raya.chuhnova@yandex.ru>
2026-03-23 23:38:32 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,24 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+    application
+}
 
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit5"))
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass.set("analizator.Week2ConsoleApp")
+}
