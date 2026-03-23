067f87f1b4d2d2ea222730c217d8e19258d2bb7f
Раиса Чухнова <Raya.chuhnova@yandex.ru>
2026-03-24 00:04:19 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,25 @@
 plugins {
-    kotlin("jvm") version "2.0.21"
+    kotlin("jvm") version "2.3.20"
     application
+    kotlin("plugin.serialization") version "2.3.20"
 }
 
 repositories {
     mavenCentral()
 }
 
+val ktorVersion = "3.4.1"
+
 dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-status-pages-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:1.5.18")
+
     testImplementation(kotlin("test-junit5"))
+    testImplementation("io.ktor:ktor-server-test-host-jvm:$ktorVersion")
 }
 
 kotlin {
@@ -20,5 +31,5 @@ tasks.test {
 }
 
 application {
-    mainClass.set("analizator.Week2ConsoleApp")
+    mainClass.set("analizator.ApplicationKt")
 }
