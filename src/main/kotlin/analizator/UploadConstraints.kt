0709808dc6d1b2189517edc64f8b704d8195a8a5
Раиса Чухнова <Raya.chuhnova@yandex.ru>
2026-03-24 10:57:59 +0300

    Create UploadConstraints.kt

---
+++ src/main/kotlin/analizator/UploadConstraints.kt
@@ -0,0 +1,7 @@
+package analizator
+
+object UploadConstraints {
+    const val MAX_FASTA_SIZE_BYTES: Int = 10 * 1024 * 1024
+    const val MAX_FASTA_SIZE_MB: Int = 10
+    const val FILE_FIELD_NAME: String = "file"
+}
