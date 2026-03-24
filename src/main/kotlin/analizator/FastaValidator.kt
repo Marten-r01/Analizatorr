08a04db9fd4f3696aa6c19e1489c0af2f3a88846
Раиса Чухнова <Raya.chuhnova@yandex.ru>
2026-03-24 10:36:05 +0300

    Update FastaValidator.kt

--- src/main/kotlin/analizator/FastaValidator.kt
+++ src/main/kotlin/analizator/FastaValidator.kt
@@ -6,7 +6,7 @@ class FastaValidator {
     fun validateStructure(lines: List<String>) {
         require(lines.isNotEmpty()) { "Файл пустой" }
         require(lines.first().startsWith(">")) { "FASTA-заголовок должен начинаться с символа '>'" }
-        require(lines.count { it.startsWith(">") } == 1) { "На 2 неделе поддерживается только одна FASTA-запись" }
+        require(lines.count { it.startsWith(">") } == 1) { "На 4 неделе поддерживается только одна FASTA-запись" }
     }
 
     fun validateHeader(header: String) {
